165dc085fb49d4669e35ea9d4966a6e7b181f6c6
Charlie Stokes <chstokes7@gmail.com>
2021-04-16 21:55:41 -0500

    Successfully broke my code getting stretch mode set up!

--- server/public/client.js
+++ server/public/client.js
@@ -1,40 +1,76 @@
 $(onReady);
 
 function onReady() {
-
-    $('#plus').on('click', setPlusOperator);
-    $('#minus').on('click', setMinusOperator);
-    $('#multiply').on('click', setMultiplyOperator);
-    $('#divide').on('click', setDivideOperator);
+    // Commented out for stretch goals
+    // $('#plus').on('click', setPlusOperator);
+    // $('#minus').on('click', setMinusOperator);
+    // $('#multiply').on('click', setMultiplyOperator);
+    // $('#divide').on('click', setDivideOperator);
 
     $('#clear').on('click', function (event) {
-        $('#number1').val('');
-        $('#number2').val('');
-        operator = undefined;
+        // $('#number2').val('');
+        // operator = undefined;
+
+        $('#math-here').val('');
     })
 
+    
     $('#go').on('click', function (event) {
         // console.log('clicked go');
         sendValues();
+        $('#math-here').val('');
     });
     
+    // If we refresh the page, we still want the history to appear in the DOM
     getHistory();
+    
+    // Numbers/operators/dot to append to math equation string that will go to the server
+
+    // More comfortable view of these statements
+    $('#num1').on('click', function (event) {
+        // Much like num += 1, this sets the value of the input field
+        // equal to itself and concatenates the new value to the string
+        $('#math-here').val($('#math-here').val()+'1');
+    });
+
+
+    $('#num2').on('click', function (event) {$('#math-here').val($('#math-here').val()+'2');});
+    $('#num3').on('click', function (event) {$('#math-here').val($('#math-here').val()+'3');});
+    $('#num4').on('click', function (event) {$('#math-here').val($('#math-here').val()+'4');});
+    $('#num5').on('click', function (event) {$('#math-here').val($('#math-here').val()+'5');});
+    $('#num6').on('click', function (event) {$('#math-here').val($('#math-here').val()+'6');});
+    $('#num7').on('click', function (event) {$('#math-here').val($('#math-here').val()+'7');});
+    $('#num8').on('click', function (event) {$('#math-here').val($('#math-here').val()+'8');});
+    $('#num9').on('click', function (event) {$('#math-here').val($('#math-here').val()+'9');});
+    $('#num0').on('click', function (event) {$('#math-here').val($('#math-here').val()+'0');});
+    // Special case of the decimal place
+    $('#dot').on('click', function (event) {$('#math-here').val($('#math-here').val()+'.');});
+    // Math operators
+    $('#plus').on('click', function (event) {$('#math-here').val($('#math-here').val()+'+');});
+    $('#minus').on('click', function (event) {$('#math-here').val($('#math-here').val()+'-');});
+    $('#multiply').on('click', function (event) {$('#math-here').val($('#math-here').val()+'*');});
+    $('#divide').on('click', function (event) {$('#math-here').val($('#math-here').val()+'/');});
+
+    // $('#input-fields').on('click', function (event) {
+    //     $('#num1').on('click', $('#math-here').append('1'))
+    // })
+
 }
 
-let operator;
-
-function setPlusOperator() {
-    operator = '+';
-}
-function setMinusOperator() {
-    operator = '-';
-}
-function setMultiplyOperator() {
-    operator = '*';
-}
-function setDivideOperator() {
-    operator = '/';
-}
+// Commented out for the stretch goals
+// let operator;
+// function setPlusOperator() {
+//     operator = '+';
+// }
+// function setMinusOperator() {
+//     operator = '-';
+// }
+// function setMultiplyOperator() {
+//     operator = '*';
+// }
+// function setDivideOperator() {
+//     operator = '/';
+// }
 
 
 
@@ -64,30 +100,26 @@ function setDivideOperator() {
 // }
 
 function sendValues() {
-    if (operator === undefined) {
-        alert('Please choose a Math operator ( + - * / )')
-    } else {
-        let newValues = {
-            number1: $('#number1').val(),
-            number2: $('#number2').val(),
-            newOperator: operator,
-            answer: 0
-        }
-      
-        $.ajax({
-            method: 'POST',
-            url: '/history',
-            data: newValues,
-        })
-        .then(function (response) {
-            // console.log('added problem');
-            getHistory();
-        })
-        .catch( function (error) {
-            console.log('error from server', error);
-            alert('sorry, could not get guesses. Try again later.');
-        })
+    
+    let newValues = {
+        mathExpression: $('#math-here').val(),
+        answer: 0
     }
+      
+    $.ajax({
+        method: 'POST',
+        url: '/history',
+        data: newValues,
+    })
+    .then(function (response) {
+        // console.log('added problem');
+        getHistory();
+    })
+    .catch( function (error) {
+        console.log('error from server', error);
+        alert('sorry, could not get guesses. Try again later.');
+    })
+    
 }
 
 function getHistory() {
